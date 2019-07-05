8bf80e60c4762995bde182644652729c8c6d579d
Aaron Smith <arabellekids@gmail.com>
2019-07-05 18:01:12 -0400

    added NavMesh compoments

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour {
+
+    NavMeshAgent agent;
+    public Animator anim;
+
+    // Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                agent.SetDestination(hitInfo.point);
+            }
+        }
+	}
+}
